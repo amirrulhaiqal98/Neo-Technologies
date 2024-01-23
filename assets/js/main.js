d04983fa7f49bb0f461e9af79704bcb7095115a3
amirrulhaiqal98 <amirrulhaiqal98@gmail.com>
2024-01-23 16:27:32 +0800

    adding rating section and counting animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -255,3 +255,20 @@
   });
 
 })()
+
+//js for counting number at rating
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.innerText = "0";
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+    const increment = target / 200;
+    if (count < target) {
+      counter.innerText = `${Math.ceil(count + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else counter.innerText = target;
+  };
+  updateCounter();
+});
